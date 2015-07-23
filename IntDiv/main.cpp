95bbf6f4a08fcbf8fb429e9622aca6d82d15eddd
Taras <targan@ukr.net>
2015-07-24 01:01:43 +0300

    IntegerDivision

---
+++ IntDiv/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	int value = 0;
+	std::cin >> value;
+	std::cout << '1' << std::endl;
+	for (int i = 2; i <= value / 2; i++)
+	{
+		if (value%i == 0)
+			std::cout << i << std::endl;
+	}
+	std::cout << value << std::endl;
+}
